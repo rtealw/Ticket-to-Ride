526b262cc197df5bed71a042990bebc8a2b4cc02
rtealw <r.teal.witter@gmail.com>
2019-10-23 10:38:27 -0400

    modularize

--- analyze_graph.py
+++ analyze_graph.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import csv
 from resistance import find_resistance
+from generate_figures import generate_figure
 from scipy.special import comb as choose
 import matplotlib.pyplot as plt
 
@@ -23,37 +24,6 @@ def find_resistance_between_pairs():
         pair_resistance = find_resistance(resistance_graph, index1, index2)
         pair["resistance"] = pair_resistance
     return pairs
-
-def generate_figure(pairs):
-    xs, ys = [], []
-    for pair in pairs:
-        x, y, number = pair['resistance'], pair['min_path'], pair['number']
-        xs.append(x)
-        ys.append(y)
-        label = ('{} {}/{}'.format(number, pair['city1'], pair['city2'])).upper()
-        plt.scatter(x,y, s=2, label=label)
-        plt.annotate(number, (x,y), ha='center', va='center', fontsize=8,
-                           bbox=dict(boxstyle="circle,pad=0.3", fc="white"))
-
-    interval = np.linspace(min(xs), max(xs), 100)
-    best_fit_func = np.poly1d(np.polyfit(xs, ys, deg=1))
-    plt.plot(interval, best_fit_func(interval), color="black")
-
-#leg = ax.legend(handlelength=0, handletextpad=0, fancybox=True)
-#for item in leg.legendHandles:
-#    item.set_visible(False)
-
-    plt.xlim(min(xs) * .5, max(xs) * 1.1)
-    plt.ylim(min(ys) * .5, max(ys) * 1.1)
-    plt.yticks(range(min(ys), max(ys)+1, 2)) # integer y axis
-    plt.title("Destination Tickets by Reward and Difficulty")
-    plt.xlabel("Effective Resistance")
-    plt.ylabel("Length of Minimum Path")
-    legend = plt.legend(handlelength = 0, handletextpad=0, fontsize=7)
-    for item in legend.legendHandles:
-        item.set_visible(False)
-
-    plt.show()
 
 def find_probability_pair():
     ways_no_pair = 0
