963d4ac0cb3cc9851336baa198e3d5fad5385c0f
rtealw <r.teal.witter@gmail.com>
2020-01-18 22:03:10 -0700

    sideways visualization of rankings

--- scripts/rankings.py
+++ scripts/rankings.py
@@ -17,19 +17,23 @@ def generate_rankings(input_file='input/results.txt', output_file='../paper/figu
     total = float(len(names_by_paths))
     colors = results['colors']
     colors.reverse()
-    plt.figure(figsize=(4,5))
+#    plt.figure(figsize=(10,3))
     for name in names_by_paths:
         i = names_by_paths.index(name) + 1
         j = names_by_residuals.index(name) + 1
         color = colors[i-1]
-        xi = 0
-        yi = 1 - i / total
-        xj = 1
-        yj = 1 - j / total
+        yi = 0
+        yj = 1
+        xi = 10 * i / total
+        xj = 10 * j / total
+#        xi = 0
+#        yi = 1 - i / total
+#        xj = 1
+#        yj = 1 - j / total
         plt.plot([xi, xj], [yi, yj], c=color, linestyle='-', marker='o', markevery=[0])
         plt.plot([xi, xj], [yi, yj], c=color, linestyle='-', marker='s', markevery=[1])
-        plt.text(x=xj+.2, y=yj-.013, s=name, c='black')
-    plt.scatter(x=6,y=0,s=.001, c='white')
+        plt.text(x=xj-.1, y=yj+.4, s=name, c='black', rotation=45)
+    plt.scatter(x=xj/2,y=10*yj,s=.001, c='white')
     plt.axis('off')
     plt.savefig(output_file)
     plt.close()
